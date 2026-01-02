f1e9451f0097d3302af77c8250751bbe89a3cc24
Mario Gomez <m.gomez03016@gmail.com>
2026-01-02 17:24:35 +0100

    clang

---
+++ ros2_ws/src/simple_pkg/include/simple_pkg/simple_publisher.hpp
@@ -0,0 +1,17 @@
+#ifndef SIMPLE_PUBLISHER_HPP
+#define SIMPLE_PUBLISHER_HPP
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+class SimplePublisher : public rclcpp::Node{
+    private:
+        void timer_callback();
+        rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_;
+        rclcpp::TimerBase::SharedPtr timer_;
+        
+    public: 
+        SimplePublisher(const std::string &name);
+};
+
+#endif // SIMPLE_PUBLISHER_HPP
